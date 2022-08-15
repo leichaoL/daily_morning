bd6f8d72951270147dcd1fe37f962d6e0d46a538
Hide0nBushes <66906588+Hide0nBushes@users.noreply.github.com>
2022-08-15 23:09:06 +0800

    增加推送notion url功能

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+from notion_client import Client
+from notion_client import AsyncClient
 
 today = datetime.now()
+today_str = date.today().strftime("%Y/%m/%d")
+
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
@@ -17,6 +21,8 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
+database_id = os.environ["DATABASE_ID"]
+notion_secret = os.environ["NOTION_SECRET"]
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
@@ -43,11 +49,19 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
+def get_notion_page():
+  notion = Client(auth=notion_secret)
+  results = notion.search(query=today).get("results")
+  if len(results) > 0:
+      result = results[0]
+      page_url = result["url"]
+  return page_url
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+url = get_notion_page()
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}, "pageurl":{"value":url}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
