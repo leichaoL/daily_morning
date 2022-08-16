1085e109743a8a8219c018ba3a84453d02c0f4fe
Hide0nBushes <66906588+Hide0nBushes@users.noreply.github.com>
2022-08-16 17:28:40 +0800

    状态变更后推送

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 import math
+from typing import Text
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
@@ -7,6 +8,7 @@ import os
 import random
 from notion_client import Client
 from notion_client import AsyncClient
+import time
 
 today = datetime.now()
 today_str = date.today().strftime("%Y/%m/%d")
@@ -23,6 +25,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 database_id = os.environ["DATABASE_ID"]
 notion_secret = os.environ["NOTION_SECRET"]
+
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
@@ -49,19 +52,53 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
-def get_notion_page():
-  notion = Client(auth=notion_secret)
+def get_notion_id():
   results = notion.search(query=today_str).get("results")
   if len(results) > 0:
       result = results[0]
-      page_url = result["url"]
-  return page_url
+      page_id = result["id"]
+  return page_id
 
-client = WeChatClient(app_id, app_secret)
+def get_notion_status():
+    pages = notion.search(query=today_str).get("results")
+    if len(pages) > 0:
+        page = pages[0]
+        page_status = page["properties"]["是否发布"]["select"]["name"]
+    else:
+        page_status = "No"
+    return page_status
 
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
-url = get_notion_page()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}, "pageurl":{"value":url}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+def get_notion_text(page_id):
+    blocks = notion.blocks.children.list(block_id = page_id)["results"]
+    text = ""
+    for block in blocks:
+        type = block["type"]
+        if len(block[type]["rich_text"]) == 0:
+            text = text + "\n"
+        else:
+            text = text + "\n" + block[type]["rich_text"][0]["plain_text"]
+    return text
+
+def update():
+    old_status = get_notion_status()
+    while True:
+        new_status = get_notion_status()
+        if (new_status != old_status):
+            old_status = new_status
+            sendMsg()
+        time.sleep(30)
+
+def sendMsg():
+  client = WeChatClient(app_id, app_secret)
+
+  wm = WeChatMessage(client)
+  wea, temperature = get_weather()
+  page_id = get_notion_id()
+  text = get_notion_text(page_id)
+  data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}, "text":{"value":text}}
+  res = wm.send_template(user_id, template_id, data)
+  print(res)
+
+
+notion = Client(auth=notion_secret)
+update()
