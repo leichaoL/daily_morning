9440ff10f7ff51c82f386f662084b2c93d21ec57
Hide0nBushes <66906588+Hide0nBushes@users.noreply.github.com>
2022-08-16 19:45:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ def get_weather():
   return weather, low, high
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d") + 1
-  return delta.days
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d") 
+  return delta.days + 1
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
